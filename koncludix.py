e0aebad6f6157ddfc75a3f9e819e79997d69ccde
joergwa <waitelonis@yovisto.com>
2026-07-30 08:24:26 +0200

    Update koncludix.py

--- koncludix.py
+++ koncludix.py
@@ -147,7 +147,7 @@ def run_konclude(binary, input_file, sparql_file, output_file):
         "-o", output_file,
         "-i", input_file
     ]
-    result = subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    result = subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
     if (result.returncode != 0):
         print ("Crashed on args: sparqlfile -s ", sparql_file, " -o ", output_file, " -i ", input_file )
     return result.returncode == 0, result.returncode
